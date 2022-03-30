8a060655dc8c5daf238043bb259ba291d948895b
Weixian <43234101+weixian-zhang@users.noreply.github.com>
2022-03-30 17:27:38 +0800

    bugfix(apiserver): Fix  HealthController throw error when returning health status

--- src/ApiServer/AzW.Web.API/Controllers/HealthController.cs
+++ src/ApiServer/AzW.Web.API/Controllers/HealthController.cs
@@ -1,17 +1,16 @@
 using System;
 using AzW.Secret;
 using Microsoft.AspNetCore.Mvc;
-using Serilog.Core;
 
 namespace AzW.Web.UI.Controllers
 {
     public class HealthController : Controller
     {
-        public HealthController(WorkbenchSecret secret, Logger logger)
+        public HealthController()
         {
         }
 
-        [HttpGet("healthz")]
+        [HttpGet("health")]
         public string Alive()
         {
             return "alive";
